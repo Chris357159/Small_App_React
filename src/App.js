73fa376fda954b9aeca3636022192f36fb3296e3
Chris357159 <104145007+Chris357159@users.noreply.github.com>
2022-05-05 15:41:21 +0200

    App - add <p>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <div className="App">
       <h1 className="appTitle">App of Small Apps</h1>
-
+      <p>Christophe</p>
       <Chip
         icon={<HomeIcon />}
         label="Home"
